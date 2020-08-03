df3399db68c5944dd30596b4b959f7feb13e0440
esam <esam18ahmed@gmail.com>
2020-08-03 22:22:16 +0200

    Categories layout

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import "./Layout/components/style/Main.scss";
 //
 import NavBar from "./Layout/components/NavBar";
 import OurCarousel from "./Layout/components/OurCarousel";
+import Categories from "./Layout/components/Categories";
 
 const App = () => {
   return (
     <Container className="p-0 m-0" fluid>
       <NavBar />
       <OurCarousel />
+      <Categories />
     </Container>
   );
 };
